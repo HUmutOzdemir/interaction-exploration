b566f74cc879085588d8afb1673eab466732aaef
Halil Umut Özdemir <uoezdemir@student.ethz.ch>
2024-02-21 19:28:25 +0000

    Parameter Change

--- affordance_seg/tools/collect_dataset.sh
+++ affordance_seg/tools/collect_dataset.sh
@@ -4,7 +4,7 @@ for GPU in 0; do
     for SPLIT in 0 1 2 3 4 5 6 7 8 9 10 11 12; do
         python -m affordance_seg.collect_dset \
             --out-dir affordance_seg/data/rgb/ \
-            NUM_PROCESSES 32 \
+            NUM_PROCESSES 8 \
             LOAD interaction_exploration/cv/rgb/run_comparison/ckpt.24.pth \
             EVAL.DATASET affordance_seg/data/episode_splits/episode_split_{$GPU}_splitted/episode_split_$SPLIT.json \
             ENV.NUM_STEPS 256 \
