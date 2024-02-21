bcbcea405cac90b5b8f6ab4fb82391d2188fcea8
Halil Umut Özdemir <uoezdemir@student.ethz.ch>
2024-02-21 15:00:41 +0000

    Parameter Change

--- affordance_seg/tools/collect_dataset.sh
+++ affordance_seg/tools/collect_dataset.sh
@@ -3,7 +3,7 @@
 for GPU in 0; do
     python -m affordance_seg.collect_dset \
          --out-dir affordance_seg/data/rgb/ \
-         NUM_PROCESSES 8 \
+         NUM_PROCESSES 32 \
          LOAD interaction_exploration/cv/rgb/run_comparison/ckpt.24.pth \
          EVAL.DATASET affordance_seg/data/episode_splits/episodes_K_256_split_$GPU.json \
          ENV.NUM_STEPS 256 \
