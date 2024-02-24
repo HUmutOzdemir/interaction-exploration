294a32fcc3cd101aa9cf3b2c2a2c6c78aeec6e1d
Halil Umut Özdemir <uoezdemir@student.ethz.ch>
2024-02-24 22:42:48 +0000

    Parameter Fix

--- interaction_exploration/tools/train_comparison.sh
+++ interaction_exploration/tools/train_comparison.sh
@@ -4,7 +4,8 @@ python -m interaction_exploration.run \
     SEED 0 \
     TORCH_GPU_ID 0 \
     X_DISPLAY :0 \
-    ENV.NUM_STEPS 256 \
-    NUM_PROCESSES 16 \
+    ENV.NUM_STEPS 512 \
+    NUM_PROCESSES 8 \
+    ENV.NUM_ENV_STEPS 2000000 \
     CHECKPOINT_FOLDER interaction_exploration/cv/intexp/run_comparison/ \
     MODEL.BEACON_MODEL affordance_seg/cv/rgb_unet/epoch=07-val_loss=0.5053.ckpt
