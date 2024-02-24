db60ab389664b760e189d7238bcb31e7b951639c
Halil Umut Özdemir <uoezdemir@student.ethz.ch>
2024-02-24 22:30:40 +0000

    Intexp RL Updates

--- interaction_exploration/tools/train_comparison.sh
+++ interaction_exploration/tools/train_comparison.sh
@@ -1,9 +1,10 @@
 python -m interaction_exploration.run \
-    --config interaction_exploration/config/rgb.yaml \
+    --config interaction_exploration/config/intexp.yaml \
     --mode train \
     SEED 0 \
     TORCH_GPU_ID 0 \
     X_DISPLAY :0 \
     ENV.NUM_STEPS 256 \
     NUM_PROCESSES 16 \
-    CHECKPOINT_FOLDER interaction_exploration/cv/rgb/run_comparison/
+    CHECKPOINT_FOLDER interaction_exploration/cv/intexp/run_comparison/ \
+    MODEL.BEACON_MODEL affordance_seg/cv/rgb_unet/epoch=07-val_loss=0.5053.ckpt
