147ae8eb7844044bcb42bcfb7833073c6f3403a8
Halil Umut Özdemir <uoezdemir@student.ethz.ch>
2024-02-27 08:49:10 +0000

    Episode Split 2

--- affordance_seg/tools/collect_dataset.sh
+++ affordance_seg/tools/collect_dataset.sh
@@ -1,6 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
-for GPU in 0; do
+for GPU in 2; do
     for SPLIT in 0 1 2 3 4 5 6 7 8 9 10 11 12; do
         python -m affordance_seg.collect_dset \
             --out-dir affordance_seg/data/rgb/ \
