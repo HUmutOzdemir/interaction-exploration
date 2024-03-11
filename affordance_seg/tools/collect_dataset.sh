e7d6ab1e25d931f840078f9ce31548929712c40d
Halil Umut Özdemir <uoezdemir@student.ethz.ch>
2024-03-11 18:33:32 +0000

    Integration of Different Seeds

--- affordance_seg/tools/collect_dataset.sh
+++ affordance_seg/tools/collect_dataset.sh
@@ -4,6 +4,7 @@ for GPU in 0; do
     for SPLIT in {1..800}; do
         timeout 2m python -m affordance_seg.collect_dset \
             --out-dir affordance_seg/data/rgb/ \
+            SEED $((GPU*800+SPLIT)) \
             NUM_PROCESSES 1 \
             LOAD interaction_exploration/cv/rgb/run_comparison/ckpt.48.pth \
             EVAL.DATASET affordance_seg/data/episode_splits/episode_split_0_splitted/episode_split_0.json \
