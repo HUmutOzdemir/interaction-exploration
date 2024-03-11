3bde4b9f8fae060aad50ab80bff3ce8966ba0e90
Halil Umut Özdemir <uoezdemir@student.ethz.ch>
2024-03-11 17:48:09 +0000

    Parameter Change

--- affordance_seg/tools/collect_dataset.sh
+++ affordance_seg/tools/collect_dataset.sh
@@ -2,9 +2,9 @@
 
 for GPU in 0; do
     for SPLIT in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24; do
-        timeout 30m python -m affordance_seg.collect_dset \
+        timeout 60m python -m affordance_seg.collect_dset \
             --out-dir affordance_seg/data/rgb/ \
-            NUM_PROCESSES 2 \
+            NUM_PROCESSES 1 \
             LOAD interaction_exploration/cv/rgb/run_comparison/ckpt.48.pth \
             EVAL.DATASET affordance_seg/data/episode_splits/episode_split_${GPU}_splitted/episode_split_$SPLIT.json \
             ENV.NUM_STEPS 256 \
