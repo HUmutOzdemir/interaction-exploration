603c672bddd56138ebcfc4dff721a583f2d8767c
Halil Umut Özdemir <uoezdemir@student.ethz.ch>
2024-03-11 18:22:41 +0000

    Generate 2 by 2

--- affordance_seg/tools/collect_dataset.sh
+++ affordance_seg/tools/collect_dataset.sh
@@ -1,12 +1,12 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
 for GPU in 0; do
-    for SPLIT in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24; do
-        timeout 30m python -m affordance_seg.collect_dset \
+    for SPLIT in {1..800}; do
+        timeout 2m python -m affordance_seg.collect_dset \
             --out-dir affordance_seg/data/rgb/ \
             NUM_PROCESSES 1 \
             LOAD interaction_exploration/cv/rgb/run_comparison/ckpt.48.pth \
-            EVAL.DATASET affordance_seg/data/episode_splits/episode_split_${GPU}_splitted/episode_split_$SPLIT.json \
+            EVAL.DATASET affordance_seg/data/episode_splits/episode_split_0_splitted/episode_split_0.json \
             ENV.NUM_STEPS 256 \
             TORCH_GPU_ID 0 \
             X_DISPLAY :0 \
